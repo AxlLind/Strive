c28b3f90282e3bdd8f4d3d03abef577038416172
Axel Lindeberg <alindeb@kth.se>
2017-03-30 17:23:05 +0200

    Adds camera following

---
+++ 2Dgame/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform player;
+
+	public float lerpSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 lerpthingy = Vector2.Lerp (transform.position, player.position, lerpSpeed);
+		transform.position = new Vector3(lerpthingy.x, lerpthingy.y, transform.position.z);
+	}
+}
