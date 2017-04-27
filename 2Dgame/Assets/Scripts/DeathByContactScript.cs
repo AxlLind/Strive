f046e53e0e1cef0d5bf2fe5a90ca43156428ff14
Axel Lindeberg <alindeb@kth.se>
2017-04-27 12:29:35 +0200

    Add platform spawning and camera movement
    Add platform spawning from the top and being destroyed when moving out of
    view. Also adds camera movement upward, simulating the world moving downward.

---
+++ 2Dgame/Assets/Scripts/DeathByContactScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathByContactScript : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D other) {
+		// Called when player loses:
+		// TODO: Implement losing condition.
+	}
+}
---
+++ 2Dgame/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float diff = Camera.main.transform.position.y - this.transform.position.y;
+		if (diff > 17f) {
+			Destroy (this.gameObject);
+		}
+	}
+}
