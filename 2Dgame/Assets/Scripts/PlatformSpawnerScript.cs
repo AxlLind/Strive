ab2b4d912c4d08e4e568dfcaffeaaee8f07d7e34
Axel Lindeberg <alindeb@kth.se>
2017-04-27 16:30:41 +0200

    Add descrete spawn positions for platforms, #11
    Makes the spawn positions of the platforms random between
    8 different positions. This makes sure all jumps are theoretically
    possible.

--- 2Dgame/Assets/Scripts/PlatformSpawnerScript.cs
+++ 2Dgame/Assets/Scripts/PlatformSpawnerScript.cs
@@ -6,11 +6,12 @@ public class PlatformSpawnerScript : MonoBehaviour {
 
 	float yPosAtLastSpawn;
 	float distanceBetween = 3f;
+
 	public GameObject platformPrefab;
 	public Transform platformParent;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 	}
 
 	void Update() {
@@ -22,7 +23,7 @@ public class PlatformSpawnerScript : MonoBehaviour {
 	}
 
 	void spawnPlatform() {
-		Vector2 pos = new Vector2(transform.position.x + Random.Range(-7f, 7f), transform.position.y);
+		Vector2 pos = new Vector2(2 * Random.Range(-3, 4), transform.position.y);
 		GameObject go = Instantiate (platformPrefab, pos, Quaternion.identity);
 		go.transform.parent = platformParent;
 	}
