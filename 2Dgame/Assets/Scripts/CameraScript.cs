7536226154d32c40f709eb1f9edb498354e5389a
Axel Lindeberg <alindeb@kth.se>
2017-04-27 14:55:39 +0200

    Add score and basic death functionality
    Add score and restart when you die. Fixes some bugs with platform-spawning
    making the distance between platforms constant. This is now a very basic
    game.

--- 2Dgame/Assets/Scripts/CameraScript.cs
+++ 2Dgame/Assets/Scripts/CameraScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
+	public float standardSpeed = 0.05f;
 	public float scrollSpeed;
 	Transform transform;
+	public Rigidbody2D target;
 
 	void Awake() {
 		transform = GetComponent<Transform> ();
@@ -13,5 +15,11 @@ public class CameraScript : MonoBehaviour {
 
 	void Update() {
 		transform.position = new Vector3 (transform.position.x, transform.position.y + scrollSpeed, transform.position.z);
+		if (target.position.y - transform.position.y > 5f) {
+			float t = 0.01f;
+			scrollSpeed = Mathf.Lerp (scrollSpeed, target.velocity.y, 0.001f);
+		} else {
+			scrollSpeed = Mathf.Lerp(scrollSpeed, standardSpeed, 0.2f);
+		}
 	}
 }
---
+++ 2Dgame/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControllerScript : MonoBehaviour {
+
+	public Text scoreText;
+	int score;
+
+	// Update is called once per frame
+	void Update () {
+		UpdateScore ();
+	}
+
+	void UpdateScore() {
+		score = (int)Camera.main.transform.position.y;
+		scoreText.text = "Score: " + score;
+	}
+}
