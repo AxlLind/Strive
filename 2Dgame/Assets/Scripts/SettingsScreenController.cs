7a4f74a0e7d85417c0826d7496b15739d95dd530
Axel Lindeberg <alindeb@kth.se>
2017-04-28 12:48:33 +0200

    Add start screen and temp settings/highscore
    Adds a basic temporary start screen with functionality to switch
    between scenes. Adds temporary settings and highscore screens with
    no functionality yet.

---
+++ 2Dgame/Assets/Scripts/SettingsScreenController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsScreenController : MonoBehaviour {
+
+	public Button backButton;
+
+	void Start () {
+		backButton.onClick.AddListener (OnClickBackButton);
+	}
+
+	void OnClickBackButton() {
+		SceneManager.LoadScene ("Start_Screen");
+	}
+}
---
+++ 2Dgame/Assets/Scripts/StartScreenController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScreenController : MonoBehaviour {
+
+	public Button startButton;
+	public Button settingsButton;
+	public Button highscoreButton;
+
+	void Start() {
+		startButton.onClick.AddListener(OnClickStartButton);
+		settingsButton.onClick.AddListener(OnClickSettingsButton);
+		highscoreButton.onClick.AddListener(OnClickHighScoreButton);
+	}
+
+	void OnClickStartButton() {
+		SceneManager.LoadScene("Main_Game");
+	}
+
+	void OnClickSettingsButton() {
+		SceneManager.LoadScene("Settings_Screen");
+	}
+
+	void OnClickHighScoreButton() {
+		SceneManager.LoadScene("Highscore_Screen");
+	}
+}
