ea18b6546b088f94348a998e35a97c3cb87b7142
Axel Lindeberg <alindeb@kth.se>
2017-05-16 20:25:21 +0200

    Improves jumping cube at menus, #82
    Improves the implementation by having the jumping cube persist between
    the scenes, except the main game. This makes it so that it transfers
    smoothly between the scenes.

---
+++ 2Dgame/Assets/Scripts/MenuPlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPlayerScript : MonoBehaviour {
+	static bool alreadyExists;
+
+	// Use this for initialization
+	void Start () {
+		if (alreadyExists) {
+			Destroy (gameObject);
+		} else {
+			DontDestroyOnLoad (gameObject);
+			alreadyExists = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().name == "Main_Game") {
+			alreadyExists = false;
+			Destroy (gameObject);
+		}
+	}
+}
