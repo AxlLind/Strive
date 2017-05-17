a33e69f8f85adb18c0822671da69bee9f0ccb379
Jonatan Cerwall <jonatan@cerwall.se>
2017-05-17 22:06:44 +0200

    Add music to Main Screen

---
+++ 2Dgame/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    static bool alreadyExists;
+
+	void Start () {
+		if (alreadyExists)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            alreadyExists = true;
+        }
+	}
+	
+	void Update () {
+        if (SceneManager.GetActiveScene().name == "Start_Screen")
+        {
+            alreadyExists = false;
+            Destroy(gameObject);
+        }
+    }
+}
